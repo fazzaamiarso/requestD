34ec821218f6e2a32b723fb88cf576f0c38fb5d2
fazzaamiarso <fazzaamiarso@gmail.com>
2022-07-26 02:13:31 +0700

    feat: Able to limit request

--- src/pages/submission/[id].tsx
+++ src/pages/submission/[id].tsx
@@ -9,6 +9,7 @@ import { SearchIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { InboxInIcon } from "@heroicons/react/outline";
 import { useEffect } from "react";
+import musicIllustration from "../../assets/happy-music.svg";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
@@ -36,25 +37,40 @@ export const getServerSideProps = async ({
       data: { status: "ENDED" },
     });
   }
-  //TODO: Do the request limit check
+
+  let requestsLeft: number | null = null;
+  if (submission && submission.personRequestLimit) {
+    const submissionToken = req.cookies["submission-token"];
+    const requestCount = await prisma.requestedTrack.count({
+      where: { request_token: submissionToken },
+    });
+    requestsLeft = submission.personRequestLimit - requestCount;
+  }
+
   submission = JSON.parse(JSON.stringify(submission));
   return {
     props: {
       submission,
+      requestsLeft,
     },
   };
 };
 
 const Submission = ({
   submission,
+  requestsLeft,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   if (!submission) return <h1>No submission with this Id found!</h1>;
 
+  if (requestsLeft && requestsLeft === 0)
+    return <h1>You have used all your request</h1>;
   if (submission.status === "ENDED") return <h1>Submission has ended.</h1>;
   if (submission.status === "PAUSED")
     return <h1>Submission has been paused by the owner.</h1>;
 
-  return <SubmissionContent submission={submission} />;
+  return (
+    <SubmissionContent submission={submission} requestsLeft={requestsLeft} />
+  );
 };
 
 type RequestInput = inferMutationInput<"request.request">;
@@ -62,6 +78,7 @@ type RequestInput = inferMutationInput<"request.request">;
 let firstRender = true;
 const SubmissionContent = ({
   submission,
+  requestsLeft,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
   const { id } = router.query;
@@ -84,10 +101,15 @@ const SubmissionContent = ({
         <title>Live Submission | Spotify - NGL</title>
       </Head>
       <header className="mx-auto my-8 w-10/12 max-w-xl">
-        <h1 className="text-2xl font-bold">{id} Live submission</h1>
+        <h1 className="text-2xl font-semibold ">{id} Live submission</h1>
         {submission?.endsAt && (
-          <span className="text-textBody">
+          <span className="text-sm text-textBody">
             submission will ends {dayjs(submission.endsAt).fromNow(false)}
+          </span>
+        )}
+        {requestsLeft && submission?.personRequestLimit && (
+          <span className="text-sm text-textBody">
+            {requestsLeft}/{submission.personRequestLimit} request left
           </span>
         )}
         <div className="mt-4 h-px w-full bg-cardBg" />
@@ -95,7 +117,13 @@ const SubmissionContent = ({
       <main className="mx-auto mt-8 w-10/12  max-w-xl ">
         <div className="flex w-full flex-col space-y-6">
           <div className="text-center ">
-            <h2 className="text-xl font-semibold">Request a song</h2>
+            <Image
+              src={musicIllustration}
+              alt="an illustration of a bird listening to music with headphone on"
+              width={150}
+              height={150}
+            />
+            <h2 className="mt-8 text-xl font-semibold">Request a song</h2>
             <p className="text-textBody">
               Search for a song and click on the request button
             </p>
@@ -130,38 +158,19 @@ const SubmissionContent = ({
             </div>
           </form>
         </div>
-        {firstRender && <RecommendedTracks onRequest={handleRequest} />}
+        {firstRender && <NewReleases onRequest={handleRequest} />}
         {mutation.data && (
           <ul className="my-6 space-y-4">
             {mutation.data.map((item) => {
               return (
-                <li
+                <RequestCard
                   key={item.id}
-                  className="flex items-center rounded-md bg-cardBg p-4 "
-                >
-                  {item.album.images[0] && (
-                    <Image
-                      src={item.album.images[0].url}
-                      alt={item.name}
-                      height={50}
-                      width={50}
-                    />
-                  )}
-                  <div className="ml-6">
-                    <h3 className="font-semibold sm:text-lg">{item.name}</h3>
-                    <h4 className="s text-sm text-textBody">
-                      {item.artists[0]?.name}
-                    </h4>
-                  </div>
-                  <button
-                    type="button"
-                    onClick={() => handleRequest(item.id)}
-                    className="ml-auto flex items-center gap-2 rounded-md bg-inputBg  p-2 text-textBody"
-                  >
-                    <InboxInIcon className="h-7 sm:h-6" />{" "}
-                    <span className="hidden text-sm sm:inline">Request</span>
-                  </button>
-                </li>
+                  trackId={item.id}
+                  artistName={item.artists[0]?.name ?? ""}
+                  coverImage={item.album.images[0]?.url ?? ""}
+                  name={item.name}
+                  onRequest={handleRequest}
+                />
               );
             })}
           </ul>
@@ -199,7 +208,40 @@ const LoadingSpinner = ({ className = "" }: { className?: string }) => {
   );
 };
 
-const RecommendedTracks = ({
+type RequestCardProps = {
+  trackId: string;
+  coverImage: string;
+  name: string;
+  artistName: string;
+  onRequest: (trackId: string) => void;
+};
+const RequestCard = ({
+  trackId,
+  coverImage,
+  name,
+  artistName,
+  onRequest,
+}: RequestCardProps) => {
+  return (
+    <li className="flex items-center gap-2 rounded-md bg-cardBg p-4 ">
+      <Image src={coverImage} alt={name} height={50} width={50} />
+      <div className="ml-4">
+        <h3 className="font-semibold sm:text-lg">{name}</h3>
+        <h4 className="s text-sm text-textBody">{artistName}</h4>
+      </div>
+      <button
+        type="button"
+        onClick={() => onRequest(trackId)}
+        className="ml-auto flex items-center gap-2 rounded-md bg-inputBg  p-2 text-textBody"
+      >
+        <InboxInIcon className="h-7 sm:h-6" />{" "}
+        <span className="hidden text-sm sm:inline">Request</span>
+      </button>
+    </li>
+  );
+};
+
+const NewReleases = ({
   onRequest,
 }: {
   onRequest: (input: RequestInput["trackId"]) => void;
@@ -208,38 +250,19 @@ const RecommendedTracks = ({
 
   return (
     <div className="mt-8 w-full">
-      <h3 className="font-semibold">Recommended tracks</h3>
+      <h3 className="font-semibold">New Releases</h3>
       <ul className="my-6 space-y-4">
         {data?.recommendations &&
           data.recommendations.map((item) => {
             return (
-              <li
+              <RequestCard
                 key={item.id}
-                className="flex items-center gap-4 rounded-md bg-cardBg p-4 "
-              >
-                {item.album.images[0] && (
-                  <Image
-                    src={item.album.images[0].url}
-                    alt={item.name}
-                    height={50}
-                    width={50}
-                  />
-                )}
-                <div className="ml-6">
-                  <h3 className="font-semibold sm:text-lg">{item.name}</h3>
-                  <h4 className="s text-sm text-textBody">
-                    {item.artists[0]?.name}
-                  </h4>
-                </div>
-                <button
-                  type="button"
-                  onClick={() => onRequest(item.id)}
-                  className="ml-auto flex items-center gap-2 rounded-md bg-inputBg  p-2 text-textBody"
-                >
-                  <InboxInIcon className="h-7 sm:h-6" />{" "}
-                  <span className="hidden text-sm sm:inline">Request</span>
-                </button>
-              </li>
+                trackId={item.id}
+                artistName={item.artists[0]?.name ?? ""}
+                coverImage={item.album.images[0]?.url ?? ""}
+                name={item.name}
+                onRequest={onRequest}
+              />
             );
           })}
       </ul>
